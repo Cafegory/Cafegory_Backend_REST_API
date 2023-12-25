61c1e7f9ba71805d23ca201e36f48da2a084700f
robinjoon <robin980108@naver.com>
2023-12-25 20:06:29 +0900

    test(CI 실패시 PR 닫기) : 빌드 성공 시 pr 닫히지 않는지 확인

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		String a = "1ㅇ";
+		String a = "1ㅇd";
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
