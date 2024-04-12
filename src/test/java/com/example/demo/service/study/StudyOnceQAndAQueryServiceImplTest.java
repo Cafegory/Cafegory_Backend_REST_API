fce17806178fe6b875fb544966b8d777db64cd80
robinjoon <robin980108@naver.com>
2024-04-12 13:13:39 +0900

    refactor: StudyOnceQAndAQueryServiceImplTest 를 Spring에서 독립적인 테스트로 변경

--- src/test/java/com/example/demo/service/study/StudyOnceQAndAQueryServiceImplTest.java
+++ src/test/java/com/example/demo/service/study/StudyOnceQAndAQueryServiceImplTest.java
@@ -1,16 +1,9 @@
 package com.example.demo.service.study;
 
-import javax.persistence.EntityManager;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.example.demo.config.TestConfig;
 import com.example.demo.domain.cafe.Cafe;
 import com.example.demo.domain.member.Member;
 import com.example.demo.domain.member.ThumbnailImage;
@@ -18,45 +11,46 @@ import com.example.demo.domain.study.StudyOnce;
 import com.example.demo.domain.study.StudyOnceComment;
 import com.example.demo.dto.study.StudyOnceCommentResponse;
 import com.example.demo.helper.CafePersistHelper;
+import com.example.demo.helper.CafePersistHelperImpl;
 import com.example.demo.helper.MemberPersistHelper;
+import com.example.demo.helper.MemberPersistHelperImpl;
 import com.example.demo.helper.StudyOnceCommentPersistHelper;
+import com.example.demo.helper.StudyOnceCommentPersistHelperImpl;
 import com.example.demo.helper.StudyOncePersistHelper;
-import com.example.demo.helper.ThumbnailImagePersistHelper;
+import com.example.demo.helper.StudyOncePersistHelperImpl;
+import com.example.demo.mapper.MemberMapper;
+import com.example.demo.mapper.StudyOnceMapper;
+import com.example.demo.repository.cafe.InMemoryCafeRepository;
+import com.example.demo.repository.member.InMemoryMemberRepository;
+import com.example.demo.repository.study.InMemoryStudyOnceCommentRepository;
+import com.example.demo.repository.study.InMemoryStudyOnceRepository;
 
-@SpringBootTest
-@Import(TestConfig.class)
-@Transactional
 class StudyOnceQAndAQueryServiceImplTest {
-	@Autowired
-	private StudyOnceQAndAQueryService studyOnceQAndAQueryService;
-	@Autowired
-	private MemberPersistHelper memberPersistHelper;
-	@Autowired
-	private ThumbnailImagePersistHelper thumbnailImagePersistHelper;
-	@Autowired
-	private StudyOncePersistHelper studyOncePersistHelper;
-	@Autowired
-	private CafePersistHelper cafePersistHelper;
-	@Autowired
-	private StudyOnceCommentPersistHelper studyOnceCommentPersistHelper;
-	@Autowired
-	private EntityManager em;
+	public static final ThumbnailImage THUMBNAIL_IMAGE = new ThumbnailImage(1L, "a");
+	private final InMemoryStudyOnceCommentRepository studyOnceCommentRepository = new InMemoryStudyOnceCommentRepository();
+	private final StudyOnceQAndAQueryService studyOnceQAndAQueryService = new StudyOnceQAndAQueryServiceImpl(
+		studyOnceCommentRepository, new MemberMapper(), new StudyOnceMapper());
+	private final InMemoryMemberRepository memberRepository = new InMemoryMemberRepository();
+	private final MemberPersistHelper memberPersistHelper = new MemberPersistHelperImpl(memberRepository);
+	private final InMemoryStudyOnceRepository studyOnceRepository = new InMemoryStudyOnceRepository();
+	private final StudyOncePersistHelper studyOncePersistHelper = new StudyOncePersistHelperImpl(studyOnceRepository);
+	private final InMemoryCafeRepository cafeRepository = new InMemoryCafeRepository();
+	private final CafePersistHelper cafePersistHelper = new CafePersistHelperImpl(cafeRepository);
+	private final StudyOnceCommentPersistHelper studyOnceCommentPersistHelper = new StudyOnceCommentPersistHelperImpl(
+		studyOnceCommentRepository);
 
 	@Test
 	@DisplayName("카공 질문을 찾는다")
 	void searchQuestion() {
-		//given
-		ThumbnailImage thumb = thumbnailImagePersistHelper.persistDefaultThumbnailImage();
-		Member leader = memberPersistHelper.persistMemberWithName(thumb, "카공장");
-		Member otherPerson = memberPersistHelper.persistMemberWithName(thumb, "김동현");
+		Member leader = memberPersistHelper.persistMemberWithName(THUMBNAIL_IMAGE, "카공장");
+		Member otherPerson = memberPersistHelper.persistMemberWithName(THUMBNAIL_IMAGE, "김동현");
 		Cafe cafe = cafePersistHelper.persistDefaultCafe();
 		StudyOnce studyOnce = studyOncePersistHelper.persistDefaultStudyOnce(cafe, leader);
 		StudyOnceComment studyOnceComment = studyOnceCommentPersistHelper.persistDefaultStudyOnceQuestion(
 			otherPerson, studyOnce);
-		em.flush();
-		em.clear();
-		//when
+
 		StudyOnceCommentResponse response = studyOnceQAndAQueryService.searchComment(studyOnceComment.getId());
+
 		Assertions.assertThat(response).isNotNull();
 	}
 
